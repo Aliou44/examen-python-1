6088b88feb438228590ae06d52d0cce90f6ed7ab
Aliou44 <94632303+Aliou44@users.noreply.github.com>
2022-02-01 17:08:54 +0100

    Add files via upload

---
+++ Examen Python .py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  1 13:30:11 2022
+
+@author: alioubadraconde
+"""
+
+#1) creation de fonction Mathématiques#
+#création de l'algorithme de pythagore
+def pythagore(x,y):
+    return x*2 + y*2
+#vérification pour 4*2+4*=25
+print(pythagore(3,4))
+#création de l'algorithme de pythagore
+def pythagore(x,y,z):
+    return x*2 + y*2 ==z
+#vérification pour 4*2+4*=25
+print(pythagore(3,4,25))
+#création de l'algorithme de pythagore
+def pythagore(x,y):
+    return x*2 + y*2
+#vérification pour 4*2+4*=25
+print(pythagore(7,128))
+
+
+
+
+  
+  
+  
+ 
+    
+    
+    
+    
+    
